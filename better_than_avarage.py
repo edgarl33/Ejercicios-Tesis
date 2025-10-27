4dbd03499ded7c3adda6a60cbd838211f31735be
edgarl33 <edgar.lee133@gmail.com>
2025-10-26 20:57:20 -0600

    dejame subir mis cosas, hombre

---
+++ better_than_avarage.py
@@ -0,0 +1,8 @@
+def better_than_average(class_points, your_points):
+
+   for X in class_points:
+      sum_class = X + class_points
+      sum_class += X
+
+grades = [10,4,9,5,7,8,5,10]
+my_grades = 8
