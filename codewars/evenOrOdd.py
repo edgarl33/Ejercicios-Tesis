61a9316cacc38f3bd944360056f311c769e964e6
edgarl33 <edgar.lee133@gmail.com>
2025-10-26 20:59:23 -0600

    a

--- codewars/evenOrOdd.py
+++ codewars/evenOrOdd.py
@@ -5,3 +5,17 @@ def even_or_odd(number):
         return "Even"
     else:
         return "Odd"
+    
+#Intercambiar los valores de dos variables. 
+x = 'A'
+y = 'B'
+
+print(x,y)
+
+temp = y
+print(temp)
+
+y=x
+x=temp
+print(x,y )
+
